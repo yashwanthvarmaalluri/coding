4f787dbf68b45bc915ea1d464db5efce03c4021d
yashwanthvarmaalluri <72602643+yashwanthvarmaalluri@users.noreply.github.com>
2021-01-18 18:02:05 +0530

    Create DFS.java

---
+++ 18-01-2020/DFS.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    private boolean visited[];
+    private ArrayList<Integer> ans;
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+         ans=new ArrayList<Integer>();
+         visited=new boolean[V+1];
+         dfs(adj,0);
+         return ans;
+    }
+    public void dfs(ArrayList<ArrayList<Integer>> adj,Integer n){
+        visited[n]=true;
+        ans.add(n);
+        for(Integer i:adj.get(n)){
+            if(!visited[i]){
+                dfs(adj,i);
+            }
+        }
+    }
+}
