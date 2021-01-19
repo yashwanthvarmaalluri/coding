8a3c269a1946f0d14f7503f6edd737bd89bbf55f
yashwanthvarmaalluri <72602643+yashwanthvarmaalluri@users.noreply.github.com>
2021-01-19 13:24:12 +0530

    Create rotten_oranges.py

---
+++ 19-01-2021/rotten_oranges.py
@@ -0,0 +1,36 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        queue=[]
+        rowlen=len(grid)
+        collen=len(grid[0])
+        c=0
+        for i in range(rowlen):
+            for j in range(collen):
+                if(grid[i][j]==2):
+                    queue.append([i,j])
+                elif(grid[i][j]==1):
+                    c+=1
+        rowoffset=[1,0,-1,0]
+        coloffset=[0,1,0,-1]
+        time=0
+        while(len(queue)!=0):
+            flag=False
+            for f in range(len(queue)):
+                curr=queue.pop(0)
+                row=curr[0]
+                col=curr[1]
+                for g in range(4):
+                    row1=row+rowoffset[g]
+                    col1=col+coloffset[g]
+                    if(row1>=0 and row1<rowlen and col1>=0 and col1<collen and grid[row1][col1]==1):
+                        grid[row1][col1]=2
+                        queue.append([row1,col1])
+                        flag=True
+            if(flag):
+                time+=1
+        for i in range(rowlen):
+            for j in range(collen):
+                if(grid[i][j]==1):
+                    return -1
+        return time
+        
